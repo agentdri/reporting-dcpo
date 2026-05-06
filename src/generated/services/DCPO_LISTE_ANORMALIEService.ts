ccb9f739d2526a3d3849393afb874a906c1ac841
emerick <kemmegneemerick@gmail.com>
2026-05-06 11:57:39 +0100

    first commit on this branch with other update

--- src/generated/services/DCPO_LISTE_ANORMALIEService.ts
+++ src/generated/services/DCPO_LISTE_ANORMALIEService.ts
@@ -10,8 +10,6 @@ import { dataSourcesInfo } from '../../../.power/schemas/appschemas/dataSourcesI
 import { getPowerSdkInstance } from '@pa-client/power-code-sdk';
 
 
-
-
 export class DCPO_LISTE_ANORMALIEService {
   private static readonly dataSourceName = 'dcpo_liste_anormalie';
 
